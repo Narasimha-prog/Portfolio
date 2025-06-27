b0263d0c59bf73d6e6dfa821737754faa95ca2f2
LN REDDY <lnreddyreddy405@gmail.com>
2025-06-27 19:20:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, BrowserRouter as Router, Routes }  from 'react-router-dom'
+import { Home } from './pages/Home'
+import { NotFound } from './pages/NotFound'
+
+const App = () => {
+  return (
+    <>
+   <Router>
+    <Routes>
+      <Route index element={<Home/>}/>
+      <Route path='*' element={<NotFound/>}/>
+    </Routes>
+   </Router>
+    </>
+  )
+}
+
+export default App
